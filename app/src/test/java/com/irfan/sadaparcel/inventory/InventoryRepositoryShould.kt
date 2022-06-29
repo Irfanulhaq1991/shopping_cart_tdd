4ca39401373ddb2df5d3cde06fd298ff1d0db4b6
Irfan <irfan@Irfans-MacBook-Air.local>
2022-06-29 12:02:16 +0500

    IntoryRepository: the no data test is now failing for the right reason

--- app/src/test/java/com/irfan/sadaparcel/inventory/InventoryRepositoryShould.kt
+++ app/src/test/java/com/irfan/sadaparcel/inventory/InventoryRepositoryShould.kt
@@ -2,14 +2,23 @@ package com.irfan.sadaparcel.inventory
 
 import com.google.common.truth.Truth.assertThat
 import com.irfan.sadaparcel.UiStates
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class InventoryRepositoryShould {
+    private lateinit var inventoryRepository: InventoryRepository
+    @BeforeEach
+    fun setup(){
+         inventoryRepository = InventoryRepository()
+    }
+
+
+
     @Test
     fun returnNoDataSuccessState(){
         val expected = UiStates.Success("no data", emptyList<String>())
-        val result =  remoteService.getInventoryItems()
-        
+        val result =  inventoryRepository.fetchInventoryItems()
+
         assertThat(result).isEqualTo(expected)
     }
 
