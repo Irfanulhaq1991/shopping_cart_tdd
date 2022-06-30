248eed1908d07a0829d3c0fa5dda53e4dfe0c76f
Irfan <irfan@Irfans-MacBook-Air.local>
2022-06-30 20:42:34 +0500

    ShoppingCart:Failing Repository oneCartItem success test is added

--- app/src/test/java/com/irfan/sadaparcel/cart/ShoppingCartRepositoryShould.kt
+++ app/src/test/java/com/irfan/sadaparcel/cart/ShoppingCartRepositoryShould.kt
@@ -2,6 +2,8 @@ package com.irfan.sadaparcel.cart
 
 import com.google.common.truth.Truth.assertThat
 import com.irfan.sadaparcel.UiState
+import com.irfan.sadaparcel.inventory.InventoryItem
+import com.irfan.sadaparcel.inventory.InventoryItemWithQuantity
 import io.mockk.every
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -14,7 +16,11 @@ class ShoppingCartRepositoryShould{
         assertThat(result).isEqualTo(UiState.Success(message = "No data"))
     }
 
-    
+    @Test
+    fun returnSuccessStateWithOneCartItem(){
+        val result = ShoppingCartRepository().fetchCartItems()
+        assertThat(result).isEqualTo(UiState.Success(listOf(InventoryItemWithQuantity(InventoryItem("1","item1","Description",2.1),1))))
+    }
 
 
 }
