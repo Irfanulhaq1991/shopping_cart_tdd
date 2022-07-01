5d768d4c32a9a62b48206c372a3cde8b457f9cbb
Irfan <irfan@Irfans-MacBook-Air.local>
2022-07-01 10:30:41 +0500

    ShoppingCart:failing test for ShoppingCartDbServiceShould  exception throwing while fetch and adding is passed

--- app/src/main/java/com/irfan/sadaparcel/cart/ShoppingCartDbService.kt
+++ app/src/main/java/com/irfan/sadaparcel/cart/ShoppingCartDbService.kt
@@ -1,20 +1,30 @@
 package com.irfan.sadaparcel.cart
 
+import android.database.sqlite.SQLiteException
+import com.irfan.sadaparcel.inventory.AppException
 import com.irfan.sadaparcel.inventory.InventoryItemWithQuantity
 
 class ShoppingCartDbService(private val dbApiShoppingCart:ShoppingCartDatabaseApi,private val limit:Int = 50): ShoppingCartService {
 
 
     override fun fetchCartItems():List<InventoryItemWithQuantity> {
-        val cartItems = dbApiShoppingCart.getAll()
-        return cartItems
+        try {
+            return dbApiShoppingCart.getAll()
+        }catch (e:SQLiteException){
+            throw AppException("Error occurred while adding cartItems")
+        }
+
     }
 
     override fun addItemToShoppingCart(inventoryItemWithQuantity: InventoryItemWithQuantity):Boolean {
-      val cartItemCount = dbApiShoppingCart.getAll().size
-      if(cartItemCount == limit) return false
-       val itemDbId =  dbApiShoppingCart.add(inventoryItemWithQuantity)
-       return itemDbId  >= 0
+        try {
+            val cartItemCount = dbApiShoppingCart.getAll().size
+            if (cartItemCount == limit) return false
+            val itemDbId = dbApiShoppingCart.add(inventoryItemWithQuantity)
+            return itemDbId >= 0
+        }catch (e:SQLiteException){
+            throw AppException("Error occurred while adding cartItems")
+        }
     }
 
 }
